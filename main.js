1810c5ec32a0de9da2dd663b86a59d58078d3983
tinajs <chrstinekarimi350@gmail.comiiiiiiiigit addgit config --global user.email chrstinekarimi350@gmail.com>
2020-12-06 17:32:23 +0300

    responsive  email validation

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn=document.querySelector('.btn');
+const errorMessage=document.querySelector(".error-text");
+const input=document.querySelector(".input");
+function hideValidation(){
+    errorMessage.classList.remove('visible');
+    input.classList
+}
